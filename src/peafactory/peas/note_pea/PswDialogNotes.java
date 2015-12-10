d40d1bdee74e22d4d711be11df10da8628a7c679
Axel von dem Bruch <Beloumi@users.noreply.github.com>
2015-12-10 15:21:59 +0100

    Create PswDialogNotes.java

---
+++ src/peafactory/peas/note_pea/PswDialogNotes.java
@@ -0,0 +1,164 @@
+package cologne.eck.peafactory.peas.note_pea;
+
+/*
+ * Peafactory - Production of Password Encryption Archives
+ * Copyright (C) 2015  Axel von dem Bruch
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation; either version 2 of the License, 
+ * or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * See:  http://www.gnu.org/licenses/gpl-2.0.html
+ * You should have received a copy of the GNU General Public License 
+ * along with this library.
+ */
+
+/**
+ * Dialog to get the password and start the note pea. 
+ */
+
+
+import settings.PeaSettings;
+import cologne.eck.peafactory.crypto.CipherStuff;
+import cologne.eck.peafactory.peas.PswDialogBase;
+import cologne.eck.peafactory.peas.gui.PswDialogView;
+import cologne.eck.peafactory.tools.Attachments;
+import cologne.eck.peafactory.tools.Converter;
+import cologne.eck.peafactory.tools.ReadResources;
+import cologne.eck.peafactory.tools.Zeroizer;
+
+
+class PswDialogNotes extends PswDialogBase {
+
+	private static LockFrameNotes lockFrame;// = new ChangeableLockFrame();
+	
+	private static PswDialogView dialogView;
+	private static PswDialogNotes dialog;
+	
+	private PswDialogNotes() {
+		dialog = this;
+		dialogView = PswDialogView.getInstance();
+	}
+	
+	protected final static PswDialogNotes getInstance() {
+		if (dialog == null) {
+			dialog = new PswDialogNotes();			
+		} else {
+			// return existing dialog
+		}
+		return dialog;
+	}
+	
+
+	public static void main(String[] args) {						
+		
+		if(args.length > 0) {
+			PswDialogBase.setWorkingMode(args[0]);
+		}
+		initializeVariables();		
+
+		// settings:
+		PswDialogBase.setFileType("text");
+		
+		Attachments.setFileIdentifier(PeaSettings.getFileIdentifier());
+		if (CipherStuff.isBound() == true){
+			Attachments.setProgramRandomBytes(PeaSettings.getProgramRandomBytes());
+		} else { // if (PeaSettings.getBound() == false) { // initialized?
+		
+			PswDialogView.checkInitialization();
+		}
+		
+		PswDialogNotes pswDialog = PswDialogNotes.getInstance();		
+		setDialog(pswDialog);
+			
+		PswDialogNotes.dialogView.setVisible(true);
+	}
+
+	
+	@Override
+	public
+	final void startDecryption() {	
+		
+		//byte[] cipherBytes = getCipherBytes(PswDialogBase.getEncryptedFileName() );
+		byte[] cipherBytes = ReadResources.readExternFile(PswDialogBase.getEncryptedFileName());// there is only one file name	
+		
+		if (PswDialogView.isInitializing() == true) {
+			
+			// handle the salt
+			cipherBytes = initializeCiphertext(cipherBytes);
+		}
+
+		if (CipherStuff.isBound() == false){
+			
+			// get and set the attached salt:
+			handleAttachedSalt(cipherBytes);
+		}
+	
+		byte[] keyMaterial = getKeyMaterial();
+
+		if (keyMaterial == null) { // bug
+			return;
+		}
+
+		//
+		// decrypt:
+		//	
+		byte[] plainBytes = null;
+		if (PswDialogView.isInitializing() == true){
+			// one space as content:
+			plainBytes = " ".getBytes(getCharset());
+		} else {
+			// decrypt:
+			plainBytes = CipherStuff.getInstance().decrypt(cipherBytes, keyMaterial, true );
+		}
+
+		if (plainBytes == null) {
+			
+			dialogView.displayErrorMessages("Decryption failed");
+
+			PswDialogView.clearPassword();
+			return;
+		}
+
+		char[] textChars = Converter.bytes2chars(plainBytes);
+		Zeroizer.zero(plainBytes);
+
+
+		// display decrypted text: 
+		if (lockFrame == null) {
+			lockFrame = LockFrameNotes.getInstance();
+		}
+		lockFrame.setChars( textChars );
+		lockFrame.setVisible(true);		
+
+
+		PswDialogView.getView().setVisible(false);		
+		
+		if (PeaSettings.getExternFile() == true) {
+			pathFileCheck();
+		}		
+		if (PswDialogView.isInitializing() == true){
+			// there is only one file: 
+			String[] fileNames = { PswDialogBase.getEncryptedFileName()};
+			// set new password:
+			lockFrame.changePassword(plainBytes, fileNames);
+		}
+	}
+
+	@Override
+	public void clearSecretValues() {
+		if (CipherStuff.getInstance().getSessionKeyCrypt() != null) {
+			CipherStuff.getInstance().getSessionKeyCrypt().clearKeys();
+		}
+	}
+
+	// not used here:
+	@Override
+	public void preComputeInThread() {}
+	@Override
+	public String[] getSelectedFileNames() {return null;}	
+}	
